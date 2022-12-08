c21dc0dd6186091d1e8eb39afd5574cfff837f4a
Elala91 <95163597+Elala91@users.noreply.github.com>
2022-12-08 11:48:09 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+// function erzhan(x,y){
+//     if(x>y){alert(y);}
+//     else{alert(x);}
+// }
+// let a=parseInt(prompt("enter a"));
+// let b=parseInt(prompt("enter b"));
+// erzhan(a,b);
+
+// function munira(x,y){
+//     alert(x**y);
+// }
+// let c=parseInt(prompt("enter a"));
+// let d=parseInt(prompt("enter b"));
+// munira(c,d);
+
+// let age=prompt("enter your birthyear?", "XXXX");
+// age=2022-age;
+// alert("you are ${age} old.");
+
+// let fruits=["apple", 'melon','peach'];
+// for(let i=0;i<3;i++){
+//     alert(fruits[i]+"1kg");
+// }
+// let arr=["t","e","s","t"];
+// alert(arr.slice(1,3));
+// let sum=0;
+// let arr=[45,34,23,78];
+// arr.forEach(sumit);
+// alert(sum)
+// function sumit(item){
+//     sum+=item;
+// }
+// let arr=[43,8,2,77,54];
+// arr.forEach(multiply)
+// function multiply(element,index,arr)
+// { arr[index]=element*10;}
+// alert(arr);
+// let arr=[4,36,25,16];
+// alert(arr.map(Math.sqrt));
+// arr=["Banana", "Apple", "Peach", "Kiwi"];
+// arr.sort();
+// alert(arr);
+// let ar=[4,2,12,1,45,22,0];
+// ar.sort(function(a,b){return a-b});
+// alert(ar);
+// 
+// let pipi = ["adidas","nike","reebok"]
+// let loloLengths = pipi.map(function(lolo){
+//     return lolo.length
+// })
+// alert(loloLengths)
+
+// let student={
+//     name:"Iskhak",
+//     age:17,
+//     height: "185cm",
+//     weight: "68kg"
+// }
+// student.sentence=function(){
+//     alert(this.name+"is   ");
+// }
+// student.sentence();
+
+let calculator={}
+let calculator.getNumbers={function(){
+    let a=parseInt(prompt());
+}}
+
+
+
+
+
+
